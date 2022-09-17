a4886d35589eb53a6ff1079958e2c559a9efa803
Darshini Venkatesha Murthy Nag <111547793+darshcloud@users.noreply.github.com>
2022-09-17 12:18:37 -0700

    code cleanup - removed commented code

--- feed/tests.py
+++ feed/tests.py
@@ -12,7 +12,7 @@ import re
 
 
 
-### Darshini Venkatesha Murthy Nag Contribution###
+# Darshini Venkatesha Murthy Nag Contribution#
 
 class ViewsTestCase(TestCase):
    
@@ -35,35 +35,29 @@ class ViewsTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         print("Test successful")
 
-        ## Darshini Venkatesha Murthy Nag Contribution##
+        # Darshini Venkatesha Murthy Nag Contribution #
 
     def test_index_delete(self):
         time.sleep(5)
         self.factory=RequestFactory()
         request = self.factory.post('',{"content":"Delete unit test tweet: "+str(datetime.datetime.now())})
         response = index(request)
-        #print(response)
-        #print(response.content)
-        #print(response.context)
         match_string = re.search("\/status\/[0-9].*\"",response.content.decode('utf-8'))
         tmp_id = match_string[0].split("/")[2].split("\"")[0]
         time.sleep(5)
         request= self.factory.post('',{"ID":str(tmp_id)})
         response=index(request)
         print("Tweet Deleted!")
-        #print(response)
         self.assertEqual(response.url, "delete.html")
         print("Test to delete tweets successful")
 
-        ## Sirisha Polisetty Contribution ##
+        # Sirisha Polisetty Contribution #
 
     def test_getQuery(self):
         self.factory = RequestFactory()
         print("Test to check if we are able to retrieve tweets as expected")
         request = self.factory.post('',{"getQuery":"Dogs"})
         response = index(request)
-        #print(response)
-        #print(response.content)
         total_count = len(re.findall("\<li\>",response.content.decode('utf-8')))
         self.assertTrue(total_count>0)
         print("Tweets retrived successfully, test successful")
